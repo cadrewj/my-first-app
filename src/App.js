0ec862bab19706c8a61c9a4910f68f8968f122aa
cadrewj <cadrewj@users.noreply.github.com>
2019-03-28 22:50:26 +0800

    react ninja

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Ninjas from './Ninjas';//adding the ninja file to the dom so it can be displayed on screen
+import AddNinja from './AddNinja';
+
+
 
 class App extends Component {
+  state = {
+    ninjas : [ //area of ninjas
+      {name: "James", age: 29, belt: "Gold",id: 1},
+      {name: "MNP", age: 25, belt: "Black",id: 2},
+      {name: "Crystral", age: 22, belt: "Brown",id: 3}
+
+    ]
+  }
+  addNinja = (ninja) =>{ //function to set the values of the new ninja
+    console.log(ninja);
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]; //"let" give you the ability to declare loacally
+    //... allows you to copy the old array and add a new feild to whats there (ninja) 
+    this.setState({ninjas: ninjas}); // assign the new ninja to the state to update the page
+  }
+  deleteNinja = (id) =>{
+   
+  // let ninjas = this.state.ninjas.filter(ninja =>{
+  //    return(ninja.id!==id);//if false it filters the ninja out of the array
+   // });
+   // this.setState({ninjas: ninjas});
+    console.log(id);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>My First React App</h1>
+        {/*this is how you nest Components*/}
+        {/*<Ninjas name="James" age="25" belt="Gold"/>*/}{/*creating props/property names to pass*/}
+      
+        <Ninjas ninjas={this.state.ninjas}/>{/*used to ouput the ninjas onto the DOM/Html page*/}
+        <br/>
+        <AddNinja addNinja = {this.addNinja} deleteNinja={this.deleteNinja}/>{/*used to ouput the form Add Ninjas onto the DOM/Html page*/}
+        {/*passing the prop to make it possible to delete a ninja
+        passing the new ninja in as a prop
+        note: the prop contains the new values for the ninja
+        */}
       </div>
     );
   }
---
+++ src/Ninjas.js
@@ -0,0 +1,48 @@
+import React from 'react'; //this is needed to create a component
+
+const Ninjas = ({ninjas, deleteNinja}) =>{ //funcrtional component, use them when you dont need states 
+    //console.log(this.props);
+    //const {name, age, belt} = this.props;
+    //const{ninjas} = this.props;//grabs the ninjas
+    //const{ninjas} = props;
+    //const ninjaList = ninjas.map(ninja => {
+
+        // if(ninja.age>22){
+        //     return (
+                
+        //         <div className= "ninjas" key= {ninja.id}>{/* key is used to make each ninja unique*/}
+        //             <div>{ninja.name}</div>
+        //             <div>{ninja.age}</div>
+        //             <div>{ninja.belt}</div>
+        //         </div>
+        //     );
+        // }
+        // else{
+        //     return null;
+        // }
+   // });// to map the ninjas for export
+   
+    return(
+        <div className= "ninja-List">
+           {/* {ninjaList}*/}{/* another way to do the same thing <div>{this.props.belt}</div> */}
+            {
+                ninjas.map(ninja =>{ //conditional output using the ternery operator 
+                    //eg: condition ? (true return this) : (false return this) 
+                    return (ninja.age > 23 ? (
+                            <div className= "ninja" key= {ninja.id}>{/* key is used to make each ninja unique*/}
+                                <div>{ninja.name}</div>
+                                <div>{ninja.age}</div>
+                                <div>{ninja.belt}</div>
+                                {/*the extra arrow function is used to stop the function from being called until u click the button*/}
+                               <button onClick={() => this.deleteNinja(ninja.id)}>X</button>
+                              
+                                </div>
+                        ) : (null)
+                    );
+                })
+            }
+        </div>       
+    );
+    
+}
+export default Ninjas;
